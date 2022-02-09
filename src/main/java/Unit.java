ef87b05ccc9014694a23177cd21fb25bf3edc8f1
Pedro Pablo Cardona Arroyave <pedropca@stud.ntnu.no>
2022-02-09 11:24:08 +0100

    JavaDoc til abstract class Unit was added

--- src/main/java/Unit.java
+++ src/main/java/Unit.java
@@ -1,12 +1,37 @@
+/**
+ *
+ * Abstract class unit that will be used as superclass to other classes.
+ * Unit class represent the most basic elements of all units in the war games.
+ * This class has a name, as private final string variable,
+ * health, attack, and armor, as private int variables.
+ *
+ * @author Pedro Cardona
+ * @version 1.0
+ * @since 1.0
+ */
+
 public abstract class Unit {
+
     private final String NAME;
     private int health;
     private int attack;
     private int armor;
 
+    /**
+     *Constructor for Class unit that has name, health, attack and armor as parameter.
+     * @param NAME String name of the unit.
+     * @param health int health points of the unit.
+     * @param attack int attack point of the unit.
+     * @param armor int armor point of the unit.
+     * @throws IllegalArgumentException Construct may throw illegal argument exception
+     * if parameter name is empty or if the parameter health is negative.
+     */
     public Unit(String NAME, int health, int attack, int armor) throws IllegalArgumentException {
-        if(health<0)throw new IllegalArgumentException("The health of a unit cannot be lower than 0.");
-        this.NAME = NAME;
+        if (NAME.isEmpty())throw new IllegalArgumentException
+                ("All unit must have a name. Define a name for the unit.");
+        if(health<0)throw new IllegalArgumentException
+                ("The health of a unit cannot be lower than 0. Define the health points above 0.");
+        this.NAME = NAME.trim();
         this.health = health;
         this.attack = attack;
         this.armor = armor;
@@ -32,22 +57,35 @@ public abstract class Unit {
         this.health = health;
     }
 
+    /**
+     * Abstract method that will define the attack bonus of the unit.
+     * This method must be defined in subclasses.
+     * @return int attack bonus of the unit
+     */
     public abstract int getAttackBonus();
+    /**
+     * Abstract method that will define the resist bonus of the unit.
+     * This method must be defined in subclasses.
+     * @return int resist bonus of the unit
+     */
     public abstract int getResistBonus();
 
+    /**
+     * Method attack that will calculate the total damage makes by the main unit
+     * to the opponent unit and set the new health points into the opponent unit.
+     * @param opponent
+     */
     public void attack(Unit opponent){
         int newHealth= opponent.getHealth() -(this.getAttack()+this.getAttackBonus())+
-                (opponent.getAttack()+opponent.getResistBonus());
+                (opponent.getArmor()+opponent.getResistBonus());
         opponent.setHealth(Math.max(newHealth, 0));
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Name of unit: ").append(this.getNAME()).append("\n");
-        sb.append("Current health points of unit: ").append(this.getHealth()).append("\n");
-        sb.append("Attack points of the unit: ").append(this.getAttack()).append("\n");
-        sb.append("Armor points of the unit: ").append(this.getArmor()).append("\n");
-        return sb.toString();
+        return "Name of unit: " + this.getNAME() + "\n" +
+                "Current health points of unit: " + this.getHealth() + "\n" +
+                "Attack points of the unit: " + this.getAttack() + "\n" +
+                "Armor points of the unit: " + this.getArmor() + "\n";
     }
 }
