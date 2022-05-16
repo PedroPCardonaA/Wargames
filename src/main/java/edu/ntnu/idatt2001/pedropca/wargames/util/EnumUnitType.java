963ee5d2e4534069c8c630dfc27ad289ec29d711
Pedro Pablo Cardona Arroyave <pedropca@stud.ntnu.no>
2022-05-16 21:22:26 +0200

    JavaDoc for the class EnumUnitType was added to the project

--- src/main/java/edu/ntnu/idatt2001/pedropca/wargames/util/EnumUnitType.java
+++ src/main/java/edu/ntnu/idatt2001/pedropca/wargames/util/EnumUnitType.java
@@ -1,6 +1,13 @@
 package edu.ntnu.idatt2001.pedropca.wargames.util;
 
-//TODO: ADD JAVADOC AND TEST
+/**
+ * Enum class that contains the different types of units that "Wargames" application support til now.
+ * The Purpose of this class is to limit the possible units that can be made from the factory class UnitFactory.
+ *
+ * @author Pedro Cardona
+ * @version 1.0
+ * @since 1.0-SNAPSHOT
+ */
 public enum EnumUnitType {
     INFANTRY("InfantryUnit"),
     HEALER("HealerUnit"),
@@ -11,10 +18,21 @@ public enum EnumUnitType {
 
     private final String unitType;
 
+    /**
+     * Constructor of the enum class, that define the String field unitType based on the signature.
+     * @param unitType String - Unit type
+     */
     EnumUnitType(String unitType) {
         this.unitType = unitType;
     }
 
+    /**
+     * Static method that compare the actual string contained into unitType field, with
+     * a sting defined in the signature. If they are equals, method will return the correspondent
+     * enum value with the correct unit type.
+     * @param nameString String - name of the unit type to be tested.
+     * @return EnumUnitType value- the correspondent enum value with the correct unit type.
+     */
     public static EnumUnitType getUnitType(String nameString){
         for(EnumUnitType enumUnitType : values()){
             if(enumUnitType.unitType.equals(nameString)) return enumUnitType;
