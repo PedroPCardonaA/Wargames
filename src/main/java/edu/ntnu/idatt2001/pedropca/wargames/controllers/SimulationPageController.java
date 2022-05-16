916919c2a155f98d8df414d2aa27d7814edfe213
Pedro Pablo Cardona Arroyave <pedropca@stud.ntnu.no>
2022-05-16 15:17:53 +0200

    FXML file SimulationPage is connected to SimulationPageController

--- src/main/java/edu/ntnu/idatt2001/pedropca/wargames/controllers/SimulationPageController.java
+++ src/main/java/edu/ntnu/idatt2001/pedropca/wargames/controllers/SimulationPageController.java
@@ -1,4 +1,30 @@
 package edu.ntnu.idatt2001.pedropca.wargames.controllers;
 
-public class SimulationPageController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SimulationPageController extends Controller implements Initializable {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextArea textArea;
+
+    private  String text = "Hello World";
+
+    @Override
+    protected void updateView() {
+        textArea.setText(text);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.updateView();
+    }
 }
