92c5f5b450941e5eb627235c29ecf58f78b2c3e2
Pedro Pablo Cardona Arroyave <pedropca@stud.ntnu.no>
2022-05-19 20:15:56 +0200

    Animation for the bar chart was added

--- src/main/java/edu/ntnu/idatt2001/pedropca/wargames/controllers/TestController.java
+++ src/main/java/edu/ntnu/idatt2001/pedropca/wargames/controllers/TestController.java
@@ -1,16 +1,21 @@
 package edu.ntnu.idatt2001.pedropca.wargames.controllers;
 
 import edu.ntnu.idatt2001.pedropca.wargames.models.Army;
+import edu.ntnu.idatt2001.pedropca.wargames.models.Battle;
+import edu.ntnu.idatt2001.pedropca.wargames.models.units.magicUnits.MagicianUnit;
 import edu.ntnu.idatt2001.pedropca.wargames.util.SingletonArmies;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.jetbrains.annotations.NotNull;
 
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.ResourceBundle;
 //TODO: JavaDOC
 //TODO: CODE SIMULATION.
@@ -18,7 +23,7 @@ public class TestController extends Controller implements Initializable {
     @FXML
     final CategoryAxis xAxis = new CategoryAxis();
     @FXML
-    final NumberAxis yAxis = new NumberAxis();
+    final NumberAxis yAxis = new NumberAxis(100,110,10);
     @FXML
     private StackedBarChart<String,Number> barChart = new StackedBarChart<String,Number>(xAxis,yAxis);
 
@@ -83,12 +88,11 @@ public class TestController extends Controller implements Initializable {
     @FXML
     private final XYChart.Series<String,Number> commanderSeries= new XYChart.Series<String,Number>();
 
-    private final Army army1= SingletonArmies.getSingletonArmies().getArmy(0);
-    private final Army army2= SingletonArmies.getSingletonArmies().getArmy(1);
+    private Army army1= SingletonArmies.getSingletonArmies().getArmy(0);
+    private Army army2= SingletonArmies.getSingletonArmies().getArmy(1);
 
     private final int startNumbersOfUnitsFromArmyOne = army1.getAllUnits().size();
     private final int startNumbersOfUnitsFromArmyTwo = army2.getAllUnits().size();
-
 
 
 
@@ -98,7 +102,43 @@ public class TestController extends Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         barChart.getXAxis().setLabel("Armies");
         barChart.getYAxis().setLabel("Unit alive %");
+        barChart.getData().addAll(infantrySeries,rangedSeries,cavalrySeries,magicianSeries,healerSeries,commanderSeries);
         this.updateView();
+
+        yAxis.setAutoRanging(false);
+        xAxis.setCategories(FXCollections.observableList(Arrays.asList(army1.getName(),army2.getName())));
+        infantrySeries.setName("Infantry");
+        cavalrySeries.setName("Cavalry");
+        magicianSeries.setName("Magician");
+        healerSeries.setName("Healer");
+        commanderSeries.setName("Commander");
+
+        Battle battle = new Battle(army1,army2);
+        Thread thread = new Thread(()->{
+            Runnable updater = ()->{
+
+            };
+            while (true){
+                try {
+                    battle.singularBattle();
+                    army1 =battle.getArmy1();
+                    army2 =battle.getArmy2();
+                    this.updateArmyInBothListInTheSingleton(army1,0);
+                    this.updateArmyInBothListInTheSingleton(army2,1);
+                    Thread.sleep(2000);
+                    if(!(army1.hasUnit() && army2.hasUnit())){
+                        break;
+                    }
+                } catch (InterruptedException e) {
+                    this.showError("Error by simulation","Error by medium simulation!",e.getMessage());
+                }
+                //Update information in the barChart was more complicated that expected
+                // and the result was not smooth as expected but good enough I think :D.
+                Platform.runLater(this::updateView);
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
     }
 
     @Override
@@ -120,51 +160,50 @@ public class TestController extends Controller implements Initializable {
         healerArmy2.setText(army2.getHealerUnits().size() +"");
         commanderArmy2.setText(army2.getCommanderUnits().size()+"");
 
-        xAxis.setCategories(FXCollections.observableList(Arrays.asList(army1.getName(),army2.getName())));
+        infantrySeries.getData().removeAll(Collections.singleton(infantrySeries.getData().setAll()));
+        rangedSeries.getData().removeAll(Collections.singleton(rangedSeries.getData().setAll()));
+        cavalrySeries.getData().removeAll(Collections.singleton(cavalrySeries.getData().setAll()));
+        commanderSeries.getData().removeAll(Collections.singleton(commanderSeries.getData().setAll()));
+        healerSeries.getData().removeAll(Collections.singleton(healerSeries.getData().setAll()));
+        magicianSeries.getData().removeAll(Collections.singleton(magicianSeries.getData().setAll()));
 
-        infantrySeries.setName("Infantry");
+
         infantrySeries.getData().add(new XYChart.Data<String,Number>(army1.getName(),getPercentOfAliveInfantryUnits(army1,startNumbersOfUnitsFromArmyOne)));
         infantrySeries.getData().add(new XYChart.Data<String,Number>(army2.getName(),getPercentOfAliveInfantryUnits(army2,startNumbersOfUnitsFromArmyTwo)));
 
 
-        rangedSeries.setName("Ranged");
         rangedSeries.getData().add(new XYChart.Data<String,Number>(army1.getName(),getPercentOfAliveRangedUnits(army1,startNumbersOfUnitsFromArmyOne)));
         rangedSeries.getData().add(new XYChart.Data<String,Number>(army2.getName(),getPercentOfAliveRangedUnits(army2,startNumbersOfUnitsFromArmyTwo)));
 
-        cavalrySeries.setName("Cavalry");
         cavalrySeries.getData().add(new XYChart.Data<String,Number>(army1.getName(),getPercentOfAliveCavalryUnits(army1,startNumbersOfUnitsFromArmyOne)));
         cavalrySeries.getData().add(new XYChart.Data<String,Number>(army2.getName(),getPercentOfAliveCavalryUnits(army2,startNumbersOfUnitsFromArmyTwo)));
 
-        magicianSeries.setName("Magician");
         magicianSeries.getData().add(new XYChart.Data<String,Number>(army1.getName(),getPercentOfAliveMagicianUnits(army1,startNumbersOfUnitsFromArmyOne)));
         magicianSeries.getData().add(new XYChart.Data<String,Number>(army2.getName(),getPercentOfAliveMagicianUnits(army2,startNumbersOfUnitsFromArmyTwo)));
 
-        healerSeries.setName("Healer");
+
         healerSeries.getData().add(new XYChart.Data<String,Number>(army1.getName(),getPercentOfAliveHealerUnits(army1,startNumbersOfUnitsFromArmyOne)));
         healerSeries.getData().add(new XYChart.Data<String,Number>(army2.getName(),getPercentOfAliveHealerUnits(army2,startNumbersOfUnitsFromArmyTwo)));
 
-        commanderSeries.setName("Commander");
         commanderSeries.getData().add(new XYChart.Data<String,Number>(army1.getName(),getPercentOfAliveCommanderUnits(army1,startNumbersOfUnitsFromArmyOne)));
         commanderSeries.getData().add(new XYChart.Data<String,Number>(army2.getName(),getPercentOfAliveCommanderUnits(army2,startNumbersOfUnitsFromArmyTwo)));
-
-        barChart.getData().addAll(infantrySeries,rangedSeries,cavalrySeries,magicianSeries,healerSeries,commanderSeries);
     }
-    private double getPercentOfAliveInfantryUnits(Army army, int startsUnits){
+    private double getPercentOfAliveInfantryUnits(@NotNull Army army, int startsUnits){
         return ((army.getInfantryUnits().size()* 100.0/startsUnits) );
     }
-    private double getPercentOfAliveRangedUnits(Army army, int startsUnits){
+    private double getPercentOfAliveRangedUnits(@NotNull Army army, int startsUnits){
         return  (army.getRangedUnits().size()* 100.0/startsUnits);
     }
-    private double getPercentOfAliveCavalryUnits(Army army, int startsUnits){
+    private double getPercentOfAliveCavalryUnits(@NotNull Army army, int startsUnits){
         return ((army.getCavalryUnits().size()* 100.0/startsUnits));
     }
-    private double getPercentOfAliveCommanderUnits(Army army, int startsUnits){
+    private double getPercentOfAliveCommanderUnits(@NotNull Army army, int startsUnits){
         return ((army.getCommanderUnits().size()* 100.0/startsUnits));
     }
-    private double getPercentOfAliveMagicianUnits(Army army, int startsUnits){
+    private double getPercentOfAliveMagicianUnits(@NotNull Army army, int startsUnits){
         return ((army.getMagicianUnits().size()* 100.0/startsUnits));
     }
-    private double getPercentOfAliveHealerUnits(Army army, int startsUnits){
+    private double getPercentOfAliveHealerUnits(@NotNull Army army, int startsUnits){
         return ((army.getHealerUnits().size()* 100.0/startsUnits) );
     }
 }
