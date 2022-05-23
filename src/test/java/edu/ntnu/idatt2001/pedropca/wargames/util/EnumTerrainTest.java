d9978f18b3c9e094c73bd21608f642b21d325003
Pedro Pablo Cardona Arroyave <pedropca@stud.ntnu.no>
2022-05-23 20:35:33 +0200

    Test for the enum class EnumTerrain was added to the project

--- src/test/java/edu/ntnu/idatt2001/pedropca/wargames/util/EnumTerrainTest.java
+++ src/test/java/edu/ntnu/idatt2001/pedropca/wargames/util/EnumTerrainTest.java
@@ -1,24 +1,57 @@
 package edu.ntnu.idatt2001.pedropca.wargames.util;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EnumTerrainTest {
-
-    @Test
-    void setForest() {
+    @Nested
+    class TestOfSetForestMethod{
+        @Nested
+        class positive{
+            @Test
+            void setForestAsTerrain(){
+                EnumTerrain.setForest();
+                assertEquals(EnumTerrain.FOREST,EnumTerrain.getTerrain());
+            }
+        }
     }
 
-    @Test
-    void setHill() {
+    @Nested
+    class TestOfSetHillMethod{
+        @Nested
+        class positive{
+            @Test
+            void setHillAsTerrain(){
+                EnumTerrain.setHill();
+                assertEquals(EnumTerrain.HILL,EnumTerrain.getTerrain());
+            }
+        }
     }
 
-    @Test
-    void setPlains() {
+    @Nested
+    class TestOfSetPlainsMethod{
+        @Nested
+        class positive{
+            @Test
+            void setPlainsAsTerrain(){
+                EnumTerrain.setPlains();
+                assertEquals(EnumTerrain.PLAINS,EnumTerrain.getTerrain());
+            }
+        }
     }
 
-    @Test
-    void setVolcano() {
+    @Nested
+    class TestOfSetVolcanoMethod{
+        @Nested
+        class positive{
+            @Test
+            void setVolcanoAsTerrain(){
+                EnumTerrain.setVolcano();
+                assertEquals(EnumTerrain.VOLCANO,EnumTerrain.getTerrain());
+            }
+        }
     }
+
 }
